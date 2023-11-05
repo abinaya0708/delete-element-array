4d6e8c8cb337c68c50a79bb3dbb830002f3eeffb
abinaya0708 <aabinaya2001.com>
2023-11-05 18:12:26 +0530

    array

---
+++ array.js
@@ -0,0 +1,15 @@
+document.write("Delete array element"+"<br>");
+document.write("Given array:[0,1,2,3,6,-2,4,6]"+"<br>");
+
+
+
+let a=[0,1,2,3,6,-2,4,6];
+
+let res=a.splice(4,1);
+
+console.log(a);
+
+document.write("delete 4th index element"+"<br>");
+
+document.write("------------"+"<br>");
+document.write("Output:"+a);
